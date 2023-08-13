396a5c5ded3572e4a59678c8c4b37a9307ef9d5f
Khawla Hassan <khawlakadry@gmail.com>
2023-08-13 17:33:54 +0400

    🎨 update pragma version

--- packages/hardhat/contracts/interfaces/IToken.sol
+++ packages/hardhat/contracts/interfaces/IToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @dev Interface of the ERC20 standard as defined in the EIP.
+ * @title Interface of the ERC20 standard
  */
 interface IToken is IERC20 {
     function decimals() external view returns (uint8);
